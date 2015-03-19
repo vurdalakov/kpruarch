6ab6e9850733088b82c8406a7965cca8b0020be6
vurdalakov <vurdalakov@gmail.com>
2015-03-19 13:58:12 +0200

    Download implemented

--- src/kpruarch/Application.cs
+++ src/kpruarch/Application.cs
@@ -14,12 +14,12 @@
                 Help();
             }
 
-            var id = _commandLineParser.FileNames[0];
+            _id = _commandLineParser.FileNames[0];
 
-            var directory = _commandLineParser.GetOptionString("output", ApplicationDirectory);
-            if (!Directory.Exists(directory))
+            _directory = _commandLineParser.GetOptionString("output", ApplicationDirectory);
+            if (!Directory.Exists(_directory))
             {
-                Directory.CreateDirectory(directory);
+                Directory.CreateDirectory(_directory);
             }
 
             var from = _commandLineParser.GetOptionString("from", "1941-06");
@@ -56,9 +56,42 @@
             return 0;
         }
 
+        private String _id;
+        private String _directory;
+
+        private WebClient _webClient = new WebClient();
+
         private void DownloadMonth(Int32 year, Int32 month)
         {
-            Console.WriteLine("{0}-{1}", year, month);
+            Print("{0}-{1:D2}", year, month);
+
+            var url = String.Format("http://www.kp.ru/arch/daily/great_war/{0}/{1:D2}/{2}/", year, month, _id);
+            
+            var html = _webClient.DownloadString(url);
+
+            var matches = Regex.Matches(html, @"""(http://pdf-download.kp.ru/api/pdf.bin/.*/(\d\d\d\d)/(\d\d)/(\d\d)/)""");
+            if (0 == matches.Count)
+            {
+                Console.WriteLine("No matches!");
+            }
+
+            foreach (Match match in matches)
+            {
+                if (5 == match.Groups.Count)
+                {
+                    var fileUrl = match.Groups[1].Value;
+                    var fileName = String.Format("kp_{0}_{1:D2}_{2:D2}.pdf", match.Groups[2].Value, match.Groups[3].Value, match.Groups[4].Value);
+                    var filePath = Path.Combine(_directory, fileName);
+
+                    if (File.Exists(filePath))
+                    {
+                        continue;
+                    }
+
+                    Print("{0}", fileName);
+                    _webClient.DownloadFile(fileUrl, filePath);
+                }
+            }
         }
 
         private Boolean ParseMonth(String yearMonth, out Int32 year, out Int32 month)
